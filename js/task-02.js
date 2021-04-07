e85b3f9f4696dad7d2000b9475d092b468f7d325
NadezdaGruzdeva <ng2015note4@gmail.com>
2021-04-07 14:18:55 +0200

    2 task

--- js/task-02.js
+++ js/task-02.js
@@ -7,10 +7,23 @@ const ingredients = [
   'Приправы',
 ];
 
+
 const ulEl = document.querySelector('ul#ingredients');
-ingredients.forEach(ingredient => {
-    console.log(ingredient);
-    const liEl = document.createElement('li');
-    liEl.textContent = ingredient;
-ulEl.append(liEl);
+const createListItem = ingredients.map(ingredient => {
+  const liEl = document.createElement('li');
+  liEl.style.listStyle = 'none';
+  liEl.insertAdjacentHTML(
+    'afterbegin', `<li>${ingredient}</li>`,
+  );
+  return liEl;
 });
+
+ulEl.append(...createListItem);
+
+//const ulEl = document.querySelector('ul#ingredients');
+//ingredients.forEach(ingredient => {
+//    console.log(ingredient);
+//    const liEl = document.createElement('li');
+//    liEl.textContent = ingredient;
+//ulEl.append(liEl);
+//});
